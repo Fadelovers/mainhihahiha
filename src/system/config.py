067422af08f07262ab2909be2fdeb2428cd3f40e
IriaKoticov <mrdidnew@gmail.com>
2025-12-14 01:49:45 +0300

    add module

--- src/system/config.py
+++ src/system/config.py
@@ -8,8 +8,6 @@ SECURITY_MONITOR_QUEUE_NAME = "security"
 DEFAULT_LOG_LEVEL = 2  # 1 - errors, 2 - verbose, 3 - debug
 LOG_FAILURE = 0
 LOG_ERROR = 1
-LOG_INFO  = 2
+LOG_INFO = 2
 LOG_DEBUG = 3
-CRITICALITY_STR = [
-    "ОТКАЗ", "ОШИБКА", "ИНФО", "ОТЛАДКА"
-]
+CRITICALITY_STR = ["ОТКАЗ", "ОШИБКА", "ИНФО", "ОТЛАДКА"]
